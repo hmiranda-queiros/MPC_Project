46c7f65aae8ad0857644482cc61cd1149a5ebb73
Hugo MIRANDA QUEIROS <hugomirandaqueiros@gmail.com>
2022-01-26 17:32:11 +0100

    debug 6.1

--- Deliverable_6_1/Deliverable_6_1.m
+++ Deliverable_6_1/Deliverable_6_1.m
@@ -1,3 +1,20 @@
 addpath(fullfile('src'));
 
 %% TODO: This file should produce all the plots for the deliverable
+
+Ts = 1/10; % Note that we choose a larger Ts here to speed up the simulation
+rocket = Rocket(Ts);
+
+H = 1;
+nmpc = NMPC_Control(rocket, H);
+
+% MPC reference with default maximum roll = 15 deg
+Tf = 30;
+ref = @(t_, x_) rocket.MPC_ref(t_, Tf);
+
+% MPC reference with specified maximum roll = 50 deg
+% roll_max = deg2rad(50);
+% ref = @(t_, x_) rocket.MPC_ref(t_, Tf, roll_max);
+
+x0 = zeros(12,1);
+[T, X, U, Ref] = rocket.simulate_f(x0, Tf, nmpc, ref);
--- Deliverable_6_1/RK4.m
+++ Deliverable_6_1/RK4.m
@@ -1,4 +1,4 @@
-function [x_next] = RK4(X,U,h,f)
+function [x_next] = RK4(X,U,rocket)
 %
 % Inputs : 
 %    X, U current state and input
@@ -7,11 +7,13 @@ function [x_next] = RK4(X,U,h,f)
 % Returns
 %    State h seconds in the future
 %
+h = rocket.Ts;
+
 
 % Runge-Kutta 4 integration
-% write your function here
-   k1 = f(X, U);
-   k2 = f(X+h/2*k1, U);
-   k3 = f(X+h/2*k2, U);
-   k4 = f(X+h*k3, U);
-   x_next = X + h/6*(k1+2*k2+2*k3+k4);
+
+k1 = rocket.f(X, U);
+k2 = rocket.f(X+h/2*k1, U);
+k3 = rocket.f(X+h/2*k2, U);
+k4 = rocket.f(X+h*k3, U);
+x_next = X + h/6*(k1+2*k2+2*k3+k4);
